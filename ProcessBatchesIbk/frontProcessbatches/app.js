e55d0334178060dbc8334f1ea807c2d59188bcc3
Francis Mendoza Chavez <55731671+francisgf@users.noreply.github.com>
2022-07-22 00:05:41 -0500

    add: changes path app

--- ProcessBatchesIbk/frontProcessbatches/app.js
+++ ProcessBatchesIbk/frontProcessbatches/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const path = require('path')
 const open = require('open')
 const cors = require('cors');
-const { serialize } = require('v8');
 
 const app= express()
 const port=4000;
@@ -9,15 +9,16 @@ const port=4000;
 app.listen(port,()=>{
   console.log(`El servidor esta escuchando en http://${port}/`); 
 });
+
 app.get("/app",(req, res) => {
   res.statusCode=200;
-  console.log("Run app")
-  res.send("hola mundo")
-  //res.render("index.html",{title:"hola"})
+  console.log("Run index.js")
+  app.use(express.static(__dirname));
+  res.sendFile(path.resolve(__dirname+'/index.html'))
 });
+
 startPath();
   function startPath(){
     open('http://127.0.0.1:4000/app') 
   }
-
 
