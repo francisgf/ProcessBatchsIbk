888e22aa11e77c7ac0d0bcdc99ec890c0c32d07c
Francis Mendoza Chavez <55731671+francisgf@users.noreply.github.com>
2022-07-25 01:36:16 -0500

    add: secure btn and validate conection server

--- ProcessBatchesIbk/backProcessbatches/index.js
+++ ProcessBatchesIbk/backProcessbatches/index.js
@@ -3,12 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const exec = require('child_process').exec
 const lector = require('fs');
+const { StringDecoder } = require('string_decoder');
+const { stringWriter } = require('xmlbuilder');
+var txt = '';
 
 const app = express();
 const port = 3000;
 app.use(cors());
 const url = '/app/runbatches'
-const urlFileTxt = '/app/readfiletxt'
+const urlFileTxt = '/app/readfilelog'
 const urlFileJson = '/app/readfilejson'
 const urlWriteJson = '/app/writefilejson'
 
@@ -19,54 +22,68 @@ app.listen('3000', function () {
 //-------- INI: END POINTS ------------
 app.get(url, cors(), (req, res) => {
     res.send('Processing..');
+    if (runBatch()) {
+        res.sendStatus(500)
+    } else {
+        res.sendStatus(200)
+    }
 });
 
 app.get(urlFileTxt, cors(), (req, res) => {
-    readLog()
+
+    // let string =readLog();
+    res.setHeader('Content-Type', 'text/text')
+    //res.writeHead(200)
+    res.readFileSync(readLog())
+
 });
 
 app.get(urlFileJson, cors(), (req, res) => {
-   let json= readUser();
+    let json = readUser();
     res.json(json);
 });
 
 app.get(urlWriteJson, cors(), (req, res) => {
-writeUser();
- });
+    writeUser();
+});
 
 //-------- END: END POINTS ------------
 
 function runBatch() {
-
+    let isRun = true;
     exec('start main.bat', (err, stdout) => {
         if (err) {
+            isRun = false;
             throw err;
         } else {
+            isRun = true;
             console.log("Iniciando ejecución")
         }
         console.log(stdout);
     });
+    return isRun;
 }
 
 function readLog() {
-    lector.readFile('Log_Main_10072022_2255.txt', 'utf-8', (err, data) => {
+    lector.readFile('Log_Main_22072022_1851.txt', 'utf-8', (err, data) => {
         if (err) {
             console.log('error: ', err);
             return;
         }
-        console.log(data);
-        return data;
+        //console.log(data);
+        txt = data
     });
-
+    //   console.log(txt)
+    return txt;
 }
 
 
 function readUser() {
-   let data= lector.readFileSync('./userConfig/users.json');     
-        let users = JSON.parse(data).users;
-        console.log("data---->");
-        console.log(users);  
-        return users;
+    let data = lector.readFileSync('./userConfig/users.json');
+    let users = JSON.parse(data).users;
+    console.log("data---->");
+    console.log(users);
+    return users;
 }
 
 /*
@@ -82,15 +99,15 @@ lector.readFile('./userConfig/users.json', (err, data) => {
 
 function writeUser() {
 
-let usersWrite = { 
-    name: 'Mike',
-    age: 30, 
-    gender: 'Male',
-    department: 'car',
-    car: 'Honda' 
-};
- 
-let data = JSON.stringify(usersWrite);
-lector.writeFileSync('usersWrite.json', data);
+    let usersWrite = {
+        name: 'Mike',
+        age: 30,
+        gender: 'Male',
+        department: 'car',
+        car: 'Honda'
+    };
+
+    let data = JSON.stringify(usersWrite);
+    lector.writeFileSync('usersWrite.json', data);
 }
 
--- ProcessBatchesIbk/frontProcessbatches/js/index.js
+++ ProcessBatchesIbk/frontProcessbatches/js/index.js
@@ -1,44 +1,73 @@
- 
-    const fecth = require("node-fetch")
-    const exec= require("c")('child_process').exec
-    
-    
-    function runFetch(){
-        readTxt();
-     console.log("Ejecutando request fetch..");
-     messageLogUI("Iniciando ejecución...")
-     messageLogUI("Error en  ejecución...")
-     fetch("http://localhost:3000/app/runbatches")
-    .then(promesaFetch => promesaFetch.ok)
-    .then(content=console.log(content))
-    .catch(err => console.error(err) );
-  
-    }
-  
-    function messageLogUI(ms){
-       /* var listAdd = document.createElement('li');
-        listAdd.appendChild(document.createTextNode(ms));
-        document.getElementById('log-container').appendChild(listAdd);*/     
-    }
-    
-    function clearLog(){
 
-    }
-    
-    
-    function powerOffServer(){
-        alert("Esta seguro que desea apagar el servidor?")
-        exec('node index.js', (err, stdout) =>{
-            if(err){
-                throw err;
-            }else{
-                alert("Se inició el servidorn correcamente")
-                console.log("Iniciando ejecución")
-            }     
-            console.log(stdout);      
-        });  
-    }
-    
+const fecth = require("node-fetch")
+
+function runFetch() {
+    btnDisabled();
+    console.log("Ejecutando request fetch..");
+    fetch("http://localhost:3000/app/runbatches")
+        .then(successFetch => successFetch.ok)
+        .then(success = delayBtnActive())
+        .then(success = hideMsnError())
+        .catch(err => showMsnError())
+}
+
+function messageLogUI(ms) {
+    /* var listAdd = document.createElement('li');
+     listAdd.appendChild(document.createTextNode(ms));
+     document.getElementById('log-container').appendChild(listAdd);*/
+}
+
+
+function readLog() {
+    console.log("Leyendo log")
+    fetch("http://localhost:3000/app/readfilelog")
+        .then(promesaFetch => promesaFetch.ok)
+        .then(content = console.log(content))
+        .catch(err => console.error(err));
+}
+
+function clearLog() {
+
+}
+
+
+function powerOffServer() {
+    alert("Esta seguro que desea apagar el servidor?")
+    exec('node index.js', (err, stdout) => {
+        if (err) {
+            throw err;
+        } else {
+            alert("Se inició el servidorn correcamente")
+            console.log("Iniciando ejecución")
+        }
+        console.log(stdout);
+    });
+}
+
+
+function delayBtnActive() {
+    timeoutID = window.setTimeout(btnActive, 2000);
+}
+function btnDisabled() {
+    document.getElementById('btnExecuteBat').disabled = true;
+    document.getElementById('content_spinner').innerHTML = `<div id='spinner' class="spinner"> </div>`;
+}
+
+function btnActive() {
+    document.getElementById('btnExecuteBat').disabled = false;
+    document.getElementById("spinner").remove();
+
+}
+
+function showMsnError() {
+    document.getElementById('ms-error').innerHTML = 'No se pudo conectar con el servidor';
+}
+
+function hideMsnError() {
+    document.getElementById('ms-error').innerHTML = '';
+}
+
+
 
 /*
         function readTxt(){
